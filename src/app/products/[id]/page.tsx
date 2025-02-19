4854652e372360943f3a03d4b67a0fb0ce4ba20c
skiende74 <skiende74@gmail.com>
2025-02-19 15:32:35 +0900

    feat(api): getProduct도 real DB로 마이그레이션

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,12 +1,10 @@
 import ProductCard from "@/app/products/ProductCard";
 import ProductCounter from "@/app/products/[id]/ProductCounter";
-import { Product } from "../model";
+import { supabase } from "@/workspace/supabase";
 
 export async function getProduct(id: string) {
-  const data = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/products/${id}`, {
-    cache: "force-cache",
-  });
-  return (await data.json()) as Product;
+  const data = await supabase.from("item").select("*").eq("id", Number(id)).single();
+  return data.data;
 }
 
 async function ProductDetail({ params }: { params: Promise<{ id: string }> }) {
